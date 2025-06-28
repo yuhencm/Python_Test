adb5591a4fa4454ee4cc79ac202b9c3684835316
Yuko <ucohanda@gmail.com>
2025-06-28 13:43:36 +0700

    add Test1 files

---
+++ example.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+def greet():
+    print("こんにちは")
+greet()
+
+def print_name(name):
+    print(f"わたしの名前は{name}です。")
+print_name("Yuko")
+
+def get_greet():
+    return "おはようございます"
+print(get_greet())
+
+def add(a,b):
+    return(a + b)
+print(add(3,5))
+
+    
